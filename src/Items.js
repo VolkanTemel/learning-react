7ec9b1e1de8ab66eddb09112e504431c0815474f
Volkan Temel <sapphiron07@windowslive.com>
2020-11-15 15:00:44 +0300

    Enter

--- src/Items.js
+++ src/Items.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Table } from "reactstrap";
 
 export default class Items extends Component {
-    render() {
-        return (
-            <div id="items">
-                {this.props.currentItems.map((items)=>(
-                    <li key={items.key}><img src={items.pic}></img>{items.title}<br/></li>
-                    
-                ))}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Table>
+          <thead>
+            <tr>
+              <th> </th>
+              <th>İlan Başlığı</th>
+              <th>Oda Sayısı</th>
+              <th>Metrekare</th>
+              <th>Fiyat</th>
+              <th>İl</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.currentItems.map((items) => (
+              <tr key={items.id}>
+                <td>
+                  <img src={items.pic}></img>
+                </td>
+                <td>{items.title}</td>
+                <td>{items.oda}</td>
+                <td>{items.metrekare}</td>
+                <td>{items.fiyat}</td>
+                <td>{items.sehir}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
