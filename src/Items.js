be6a4c7c036df7e683e9ebdb3e3841aa9f0764dc
Volkan Temel <sapphiron07@windowslive.com>
2020-11-15 15:44:31 +0300

    Enter

--- src/Items.js
+++ src/Items.js
@@ -10,8 +10,7 @@ export default class Items extends Component {
             <tr>
               <th> </th>
               <th>İlan Başlığı</th>
-              <th>Oda Sayısı</th>
-              <th>Metrekare</th>
+             
               <th>Fiyat</th>
               <th>İl</th>
             </tr>
@@ -23,9 +22,8 @@ export default class Items extends Component {
                   <img src={items.pic}></img>
                 </td>
                 <td>{items.title}</td>
-                <td>{items.oda}</td>
-                <td>{items.metrekare}</td>
-                <td>{items.fiyat}</td>
+            
+                <td>{items.fiyat}TL</td>
                 <td>{items.sehir}</td>
               </tr>
             ))}
